3bf7a0c6ad75a4ec94832480aab5848afea1110a
0xNike <jianhao.lm@gmail.com>
2022-08-08 23:03:38 +0800

    Updated src files

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,21 +10,36 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <div className={layoutStyles.container}>
       <title>{pageTitle}</title>
-      <nav>
-        <ul className={layoutStyles.navLinks}>
-          <li className={layoutStyles.navLinkItem}>
-            <Link to="/" activeClassName={layoutStyles.active}>
-                Home
-            </Link ></li>
-          <li className={layoutStyles.navLinkItem}>
-            <Link to="/blog" className={layoutStyles.navLinkText} activeClassName={layoutStyles.active}>
-                Blog
-            </Link></li>
-        </ul>
-      </nav>
-      <main>
+      <div className={layoutStyles.gridContainer}>
+        <div className={layoutStyles.gridItem1}>
+          <h1 className={layoutStyles.siteLogo}>0xNike</h1>
+          <p>Student Developer</p>
+        </div>
+        <nav className={layoutStyles.gridItem2}>
+          <ul className={layoutStyles.navLinks}>
+            <li className={layoutStyles.navLinkItem}>
+              <Link to="/" className={layoutStyles.navLinkText} activeClassName={layoutStyles.active}>
+                  Home
+              </Link ></li>
+            <li className={layoutStyles.navLinkItem}>
+              <Link to="/blog" className={layoutStyles.navLinkText} activeClassName={layoutStyles.active}>
+                  Blog
+              </Link></li>
+              <li className={layoutStyles.navLinkItem}>
+              <Link to="/blog" className={layoutStyles.navLinkText} activeClassName={layoutStyles.active}>
+                  Projects
+              </Link></li>
+          </ul>
+        </nav>
+        <div className={layoutStyles.gridItem3}>
+          <main>
+          {children}
+          </main>    
+        </div>
+      </div>
+      {/* <main>
         {children}
-      </main>
+      </main> */}
       <footer>
         <h2>Contact me</h2>
         <a href="https://github.com/0xNike">
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,13 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Homepage">
         {/* This will be passed as a children */}
-        <h1>I'm 0xNike.</h1>
-        <h2>Welcome to Web3</h2>
-        <p>Student Developer</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu mi, scelerisque vel mi consequat, 
+          lobortis gravida arcu. Maecenas dignissim pretium eros. Mauris iaculis ultricies turpis faucibus lacinia. 
+          Suspendisse massa justo, egestas nec leo ac, pellentesque molestie erat. Aenean ligula massa, placerat id 
+          urna nec, ultrices dignissim ante. Quisque viverra consectetur nibh, vel vulputate elit dignissim et. 
+          Duis ut pharetra nulla, non iaculis nunc. Nam aliquet hendrerit metus, ut porta nulla venenatis sit amet.
+        </p>
         {/* <StaticImage alt="Ethereum Finance" src="https://ethereum.org/static/0453c88b09ddaa2c7e7552840c650ad2/41cf9/finance_transparent.webp"/>
         <StaticImage alt="CryptoPunk #9998" src="../images/CP9998.png"/> */}
     </Layout>
