3ee005a21cd7e0816a6847ba952a74db345226ba
0xNike <jianhao.lm@gmail.com>
2022-08-08 16:48:58 +0800

    Updated website

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import * as layoutStyles from './layout.module.css'
+import "@fontsource/ibm-plex-mono"
+import "@fontsource/inter"
+import { StaticImage } from 'gatsby-plugin-image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 const Layout = ({ pageTitle, children }) => {
   return (
@@ -9,19 +14,24 @@ const Layout = ({ pageTitle, children }) => {
       <nav>
         <ul className={layoutStyles.navLinks}>
           <li className={layoutStyles.navLinkItem}>
-            <Link to="/" >
+            <Link to="/" activeClassName={layoutStyles.active}>
                 Home
             </Link ></li>
           <li className={layoutStyles.navLinkItem}>
-            <Link to="/blog" className={layoutStyles.navLinkText}>
+            <Link to="/blog" className={layoutStyles.navLinkText} activeClassName={layoutStyles.active}>
                 Blog
             </Link></li>
         </ul>
       </nav>
       <main>
-        <h1 className={layoutStyles.heading}>{pageTitle}</h1>
         {children}
       </main>
+      <footer>
+        <h2>Contact me</h2>
+        <a href="https://github.com/0xNike">
+          <FontAwesomeIcon icon={brands('github')} size="2x" inverse />
+        </a>        
+      </footer>
     </div>
   )
 }
