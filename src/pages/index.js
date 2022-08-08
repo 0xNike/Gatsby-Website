9a8566a766a83ff1efe1b623584c26339304d034
0xNike <jianhao.lm@gmail.com>
2022-08-08 18:43:59 +0800

    Updated font stack

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => {
     <Layout pageTitle="Homepage">
         {/* This will be passed as a children */}
         <h1>I'm 0xNike.</h1>
+        <h2>Welcome to Web3</h2>
         <p>Student Developer</p>
         {/* <StaticImage alt="Ethereum Finance" src="https://ethereum.org/static/0453c88b09ddaa2c7e7552840c650ad2/41cf9/finance_transparent.webp"/>
         <StaticImage alt="CryptoPunk #9998" src="../images/CP9998.png"/> */}
